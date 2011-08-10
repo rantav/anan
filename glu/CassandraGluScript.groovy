023d8489744d1e9b1a0a4af84590b97a56f35f4c
Ran Tavory <rantav@gmail.com>
2011-08-10 08:49:06 +0300

    Update cassandra's glu

--- glu/CassandraGluScript.groovy
+++ glu/CassandraGluScript.groovy
@@ -1,13 +1,32 @@
+/*
+ * Copyright (c) 2011 Yan Pujante
+ * Author: Ran Tavory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 import org.linkedin.glu.agent.api.ShellExecException
 import org.linkedin.util.io.resource.Resource
 
+/**
+ * @author rantav@gmail 
+ */
 class CassandraGluScript {
   static requires = {
     agent(version: '1.6.0')
   }
 
   // TODO: the version needs to be retained from teh tar.
-  def version = '0.8.1'
+  def version = '1.0.0'
   def serverRoot
   def logsDir
   def serverLog
@@ -55,7 +74,11 @@ class CassandraGluScript {
   def start = {
     log.info "Starting..."
     pidFile = "${logsDir}/cassandra.pid"
-    shell.exec("${serverRoot}/bin/cassandra -p ${pidFile}")
+    shell.rm(pidFile)
+    String cmd = "${serverRoot}/bin/cassandra -p ${pidFile}"
+    log.info "EXEC: ${cmd}"
+    String out = shell.exec(cmd)
+    log.info "STARTED: ${out}"
     // we wait for the process to be started (should be quick)
     shell.waitFor(timeout: '10s', heartbeat: '500') {
       pid = isProcessUp()
@@ -84,14 +107,17 @@ class CassandraGluScript {
       log.info "Cassandra is already down."
     } else {
       // invoke the stop command
-      shell.exec("kill -9 ${pid}")
-
+      String cmd = "kill -9 ${pid}"
+      log.info "EXEC: ${cmd}"
+      String out = shell.exec(cmd)
+      log.info "Killed: ${out}"
       // we wait for the process to be stopped
       shell.waitFor(timeout: params.stopTimeout, heartbeat: '1s') { duration ->
         log.info "${duration}: Waiting for server to be down"
         isProcessDown()
       }
     }
+    shell.rm(pidFile)
     pid = null
   }
 
@@ -103,7 +129,10 @@ class CassandraGluScript {
 
   private Integer isProcessUp() {
     try {
-      def output = shell.exec("${serverRoot}/bin/nodetool -h localhost ring")
+      String cmd = "${serverRoot}/bin/nodetool -h localhost ring"
+      log.info "EXEC: ${cmd}"
+      def out = shell.exec(cmd)
+      log.info "nodetool: ${out}"
       def pid = shell.cat(pidFile)
       if (!shell.listening('localhost', 9160)) {
         log.info("Cassandra not yet listening on port 9160")
